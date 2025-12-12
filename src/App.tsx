036f8b92fc79acde34c5e5c77d71767423b74e35
Bermelio <facundoperalta258@gmail.com>
2025-12-12 03:09:15 -0300

    last commit today

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/layout/Header"
 import MainLayout from "./components/layout/Main"
 import About from "./components/layout/About"
-import Contact  from "./components/layout/Contanct"
+import Contact from "./components/layout/Contact";
 import Footer from "./components/layout/Footer"
 import MoreInfo from "./components/layout/MoreInfo"
 
